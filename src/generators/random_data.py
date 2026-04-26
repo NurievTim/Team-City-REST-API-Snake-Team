37e9932023d32da471362799fe0d1c3400580530
GLskill <Glebluz1488@gmail.com>
2026-04-27 00:20:17 +0200

    Added generator and config

---
+++ src/generators/random_data.py
@@ -0,0 +1,29 @@
+import random
+import os
+import uuid
+from faker import Faker
+
+faker = Faker()
+
+
+class RandomData:
+    @staticmethod
+    def get_username() -> str:
+        # UUID гарантирует уникальность даже при параллельных запусках
+        unique_id = str(uuid.uuid4())[:8]  # Первые 8 символов UUID
+        base_name = ''.join(faker.random_letters(length=random.randint(3, 6)))
+        return f"{base_name}{unique_id}"  # Например: aBcXyz12a4f5
+
+    @staticmethod
+    def get_deposit_amount(min_value: float = 1.0, max_value: float = 100000.0) -> float:
+        return random.uniform(min_value, max_value)
+
+    @staticmethod
+    def get_password() -> str:
+        upper = [letter.upper() for letter in faker.random_letters(length=3)]
+        lower = [letter.lower() for letter in faker.random_letters(length=3)]
+        digits = [str(faker.random_digit()) for _ in range(3)]
+        special = [random.choice('!@#$%^&')]
+        password = upper + lower + digits + special
+        random.shuffle(password)
+        return ''.join(password)
