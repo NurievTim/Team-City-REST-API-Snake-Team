13a5c6dde7ec16235030994164eb5805b0753d36
nuriev_t_r <tim.nur.usa@gmail.com>
2026-05-16 18:23:02 +0300

    refactor: изменил путь в конфиге

--- src/api/configs/config.py
+++ src/api/configs/config.py
@@ -10,7 +10,7 @@ class Config:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Config, cls).__new__(cls)
-            config_path = Path(__file__).parents[2] / 'resources' / 'config.properties'
+            config_path = Path(__file__).parents[3] / 'resources' / 'config.properties'
             if not config_path.exists():
                 raise ImportError(f'{config_path}: config.properties not found')
             with open(config_path, 'r') as f:
